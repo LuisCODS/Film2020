2995e7d81d2b6bd7df31f197e177c0ef376c2a14
Luis <tennis.bresil@gmail.com>
2020-12-18 17:08:49 -0500

    panier show ok

--- FilmSPA/view/membre/template/dashbordPanier.php
+++ FilmSPA/view/membre/template/dashbordPanier.php
@@ -2,7 +2,36 @@
 session_start();
 ?>
 
-
+  <!-- ROW 1 -->
+  <div class="row mb-3">    
+      <!--  COL 1 -->
+      <div class="col-md-3">
+          <h2><i class="fas fa-cart-arrow-down"></i>   Votre Panier</h2> 
+      </div>  
+      <!--  COL 2 -->
+      <div class="col-md-6">
+      </div> 
+      <!--  COL 3 -->
+      <div class="col-md-3">
+           <a class="btn btn-outline-danger" onClick="" role="button">Vider panier</a>
+      </div> 
+  </div> 
+<!-- ROW 2 -->
+<div class="row">
+    <!--COL 1-->
+    <div class="col-md-12">
+        <!-- TABLE -->
+        <table class="table table-hover ">
+            <thead class="thead-dark">
+                <tr>
+                    <th>Pochette</th>
+                    <th>Titre</th>
+                    <th>Prix</th>
+                    <th>Quantite</th>
+                    <th></th>                                
+                </tr>
+            </thead>
+            <tbody>  
 
 <?php
 extract($_POST);
@@ -11,17 +40,11 @@ $data = json_decode($dataJson);
 //                       <h4>Total de films (".count($data).")</h4>
 //                 </div>";
 
-echo "<pre>";
-print_r($_SESSION['panier']);
-echo "<pre>";
-
-
+// echo "<pre>";
+// print_r($_SESSION['panier']);
+// echo "<pre>";
 if (isset ($_SESSION['panier']) )
 { 
-    if(count($_SESSION['panier']) == 0)
-    {
-      echo "Votre panier est vide";
-    }else{
 
         $connection = new PDO('mysql:host=localhost;dbname=bdfilms_SPA','root','');
 
@@ -31,21 +54,19 @@ if (isset ($_SESSION['panier']) )
             $select->bindValue(1,$idFilm);
             $select->execute();
             $films = $select->fetchAll();
-
-            echo  $films[0]['titre'];
-            echo  $quantite."</br>";
-        }
-    }
-}
-
- 
-  
-
-
- 
-
-
-   
-
-
-
+?>
+    <tr>
+        <td><img src="../../img/<?php echo($films[0]['pochette'])?>" width=80 height=80></td>
+        <td><?php echo  $films[0]['titre']; ?></td>
+        <td>$ <?php echo  $films[0]['prix'];?></td>
+        <td> <?php echo  $quantite;  ?></td>                     
+    </tr>  
+<?php 
+       } 
+} 
+?>
+           </tbody>
+          </table>
+          <!-- FIN TABLE -->
+      </div>
+  </div>
