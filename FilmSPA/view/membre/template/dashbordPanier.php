4852b306454f46717b578b1e9d1e13c0c8b35ce4
Luis <tennis.bresil@gmail.com>
2020-12-19 11:25:27 -0500

    ss

--- FilmSPA/view/membre/template/dashbordPanier.php
+++ FilmSPA/view/membre/template/dashbordPanier.php
@@ -2,43 +2,24 @@
 session_start();
 ?>
 
-  <!-- ROW 1 -->
-  <div class="row mb-3">    
-      <!--  COL 1 -->
-      <div class="col-md-3">
-          <h2><i class="fas fa-cart-arrow-down"></i>   Votre Panier</h2> 
-      </div>  
-      <!--  COL 2 -->
-      <div class="col-md-6">
-      </div> 
-      <!--  COL 3 -->
-      <div class="col-md-3">
-           <a class="btn btn-outline-danger" onClick="" role="button">Vider panier</a>
-      </div> 
-  </div> 
-<!-- ROW 2 -->
-<div class="row">
-    <!--COL 1-->
-    <div class="col-md-12">
-        <!-- TABLE -->
-        <table class="table table-hover ">
-            <thead class="thead-dark">
-                <tr>
-                    <th>Pochette</th>
-                    <th>Titre</th>
-                    <th>Prix</th>
-                    <th>Quantite</th>
-                    <th></th>                                
-                </tr>
-            </thead>
-            <tbody>  
+
 
 <?php
 extract($_POST);
 $data = json_decode($dataJson);
 
+
+// echo "<pre>";
+// print_r($_SESSION['panier']);
+// echo "<pre>";
+
+
 if (isset ($_SESSION['panier']) )
 { 
+    if(count($_SESSION['panier']) == 0)
+    {
+      echo "Votre panier est vide";
+    }else{
 
         $connection = new PDO('mysql:host=localhost;dbname=bdfilms_SPA','root','');
 
@@ -71,3 +52,16 @@ if (isset ($_SESSION['panier']) )
           <!-- FIN TABLE -->
       </div>
   </div>
+
+
+            echo  $films[0]['titre'];
+            echo  $quantite."</br>";
+        }
+    }
+}
+
+ 
+  
+
+
+ 
